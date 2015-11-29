6d44bd602c73adf7f44aea17bc1d5abd00a16814
Jonathan Frederickson <jonathan@terracrypt.net>
2015-11-29 02:28:17 -0500

    Initial commit

--- mxpush.py
+++ mxpush.py
@@ -1,5 +1,25 @@
-'''
-Documentation, License etc.
+import json, requests
+from flask import Flask, jsonify, request
+import ConfigParser
+from matrix_client.api import MatrixHttpApi
+app = Flask(__name__)
 
-@package mxpush
-'''
+config = ConfigParser.ConfigParser()
+config.read('/etc/mxpush.conf')
+TOKEN = config.get("Matrix", "token")
+HOMESERVEr = config.get("Matrix", "homeserver")
+ROOMID = config.get("Matrix", "room_id")
+  
+@app.route("/", methods=["POST"])
+def handleNotification():
+  req = request.get_json()
+  print(req)
+  
+  matrix = MatrixHttpApi(HOMESERVER, token=req['notification']['devices'][0]['pushkey'])
+  print(matrix.send_message_event(room_id=ROOMID, event_type='net.terracrypt.matrix.push', content=req))
+  
+  return jsonify({})
+
+if __name__ == "__main__":
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.run(host='0.0.0.0')
